b546d6e40c2263917ff9bad334db9450887f84b0
Heera Singh <heera.madquick@gmail.com>
2025-11-12 17:42:10 +0530

    button and accordion added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./styles/globals.css"
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+export * from "./components/ui/accordion";
 
 export {
     Input,
---
+++ src/test/index.ts
@@ -0,0 +1,1 @@
+export {Button, Input } from "../../src/index"
